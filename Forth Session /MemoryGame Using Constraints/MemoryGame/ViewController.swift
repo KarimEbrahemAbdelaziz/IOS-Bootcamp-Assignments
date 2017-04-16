71841f487cd517c9648b5ea0ee26617995841047
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2017-04-16 17:21:01 +0200

    Forth Session : Memory Game Using Constraints

---
+++ Forth Session /MemoryGame Using Constraints/MemoryGame/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by KARIM on 4/14/17.
+//  Copyright © 2017 KARIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
